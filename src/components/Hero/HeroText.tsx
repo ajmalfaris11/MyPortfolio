2ad8d12725bc040db6cf01def86a5ce4fa0b16e0
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-02 16:16:57 +0530

    Seperate the hero Text from Hero Section component for better maintanability

---
+++ src/components/Hero/HeroText.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+import { Animate3DText } from "@/components/ui/animations";
+import { titles } from "@/data/sections/heroDatas";
+import { useEffect, useState } from "react";
+
+const HeroText: React.FC = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prev) => (prev + 1) % titles.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="text-center md:text-left flex-2 min-h-[150px]">
+      <p className="text-xl">Hello, It&apos;s Me</p>
+      <Animate3DText
+        text="Ajmal Faris"
+        className="text-[16vw] sm:text-[8vw] font-bold text-nowrap animate-shine"
+        rotateDepth={10}
+        translateDepth={10}
+      />
+      <div className="flex flex-col sm:flex-row justify-start items-center sm:gap-2 text-[8vw] md:text-[2.5vw] xl:text-[3.5vw] font-semibold mt-2">
+        <span className="text-white">And I'm a</span>
+        <AnimatePresence mode="wait">
+          <motion.h2
+            key={index}
+            className="text-blue-600"
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 10 }}
+            transition={{ duration: 0.3 }}
+          >
+            {titles[index].title}
+          </motion.h2>
+        </AnimatePresence>
+      </div>
+
+      <AnimatePresence mode="wait">
+        <motion.p
+          key={index}
+          className="mt-4 text-gray-300 max-w-md xl:max-w-xl text-[4vw] md:text-[2vw] lg:text-[1.3vw] xl:text-[1.5vw]"
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.3 }}
+        >
+          {titles[index].description}
+        </motion.p>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default HeroText;
