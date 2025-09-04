5a88338faf0e1cccc92b2f2770b30f7c96f0b012
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-04 13:54:32 +0530

    fix(footer-dock): make container width auto so dock is visible on small devices

--- src/components/Footer/FooterFlowtingDock.tsx
+++ src/components/Footer/FooterFlowtingDock.tsx
@@ -1,58 +1,40 @@
 import React from "react";
 import { FloatingDock } from "@/components/ui/layouts";
-import {
-  IconBrandGithub,
-  IconBrandX,
-  IconHome,
-} from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandX, IconHome } from "@tabler/icons-react";
 import { SiLeetcode } from "react-icons/si";
 import { SlSocialLinkedin } from "react-icons/sl";
 
 export function FooterFloatingDock() {
-  const links = [ 
+  const links = [
     {
       title: "Twitter",
-      icon: (
-        <IconBrandX className="h-full w-full text-blue-600" />
-      ),
+      icon: <IconBrandX className="h-full w-full text-blue-600" />,
       href: "#",
     },
     {
       title: "LinkedIn",
-      icon: (
-        <SlSocialLinkedin className="h-full w-full text-blue-600" />
-      ),
+      icon: <SlSocialLinkedin className="h-full w-full text-blue-600" />,
       href: "https://www.linkedin.com/in/ajmal-faris11/",
     },
     {
       title: "Home",
-      icon: (
-        <IconHome className="h-full w-full text-blue-600" />
-      ),
+      icon: <IconHome className="h-full w-full text-blue-600" />,
       href: "/",
     },
     {
       title: "GitHub",
-      icon: (
-        <IconBrandGithub className="h-full w-full text-blue-600" />
-      ),
+      icon: <IconBrandGithub className="h-full w-full text-blue-600" />,
       href: "https://github.com/ajmalfaris11",
     },
     {
       title: "Leetcode ",
-      icon: (
-        <SiLeetcode  className="h-full w-full text-blue-600" />
-      ),
+      icon: <SiLeetcode className="h-full w-full text-blue-600" />,
       href: "#",
     },
-    
   ];
   return (
-    <div className="flex items-center justify-center w-[10%] my-2">
-      <FloatingDock
-        mobileClassName="translate-y-10"
-        items={links}
-      />
+    <div className="flex items-center justify-center my-2 w-auto">
+      <FloatingDock mobileClassName="translate-y-10" items={links} />
     </div>
   );
 }
