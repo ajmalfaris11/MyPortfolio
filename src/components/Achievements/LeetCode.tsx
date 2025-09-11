6a7fd7bed32dd7ee12245e15b0c1511969f92e2a
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-11 18:11:47 +0530

    feat(achievements): add LeetCode component with SparklesText effect

---
+++ src/components/Achievements/LeetCode.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SparklesText } from "@/components/ui/effects/SparklesText";
+
+export default function LeetCode() {
+  return (
+    <div className="bg-transparent">
+      <SparklesText text="Leet Code" />{" "}
+    </div>
+  );
+}
