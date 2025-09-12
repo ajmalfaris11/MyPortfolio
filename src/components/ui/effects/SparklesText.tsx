2f829ab89bc96a8f7a58fc5d88c40faab9d7921f
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-12 16:10:46 +0530

    refactor(SparklesText): replace hardcoded text with dynamic prop for improved reusability

--- src/components/ui/effects/SparklesText.tsx
+++ src/components/ui/effects/SparklesText.tsx
@@ -6,7 +6,7 @@ export function SparklesText({ text }: { text: string }) {
   return (
     <div className="w-full bg-transparent flex flex-col items-center justify-center overflow-hidden rounded-md">
       <Animate3DText
-        text="Leet Code"
+        text={text}
         className="md:text-7xl text-3xl lg:text-7xl font-bold text-center animate-shine relative z-20"
         rotateDepth={10}
         translateDepth={10}
