48213262dd55587d22ea850dda052955fa5e0bca
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-12 16:10:07 +0530

    refactor(Testimonials): streamline imports and improve component structure for clarity

--- src/components/Testimonials/TestimonialsSection.tsx
+++ src/components/Testimonials/TestimonialsSection.tsx
@@ -3,31 +3,39 @@
 import React from "react";
 
 import { companies, testimonials } from "@/data";
-import { InfiniteCards } from "@/components/ui/animations";
-import { FlowCards } from "@/components/ui/animations";
+import { InfiniteCards, FlowCards, CompanyCardItem } from "@/components/ui/animations";
 import { Animate3DDiv } from "@/components/ui/animations";
 
 const Testimonials = () => {
   return (
     <section
       id="testimonials"
-      className="py-10 z-10 flex flex-col gap-10 justify-center items-center">
+      className="py-10 z-10 flex flex-col gap-10 justify-center items-center"
+    >
       <Animate3DDiv rotateDepth={10} translateDepth={10}>
         <h1 className="text-4xl sm:text-6xl font-semibold text-center">
           What <span className="text-purple animate-shine">People Say</span>
         </h1>
       </Animate3DDiv>
+
       <div className="flex flex-col items-center ">
         <div className="rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
-          <InfiniteCards
-            items={testimonials}
-            direction="right"
-            speed="slow"
-          />
+          <InfiniteCards items={testimonials} direction="right" speed="slow" />
         </div>
       </div>
+
       <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16">
-        <FlowCards items={companies} direction="left" speed="slow" />
+        <FlowCards direction="left" speed="slow">
+          {companies.map((c, idx) => (
+            <CompanyCardItem
+              key={idx}
+              img={c.img}
+              nameImg={c.nameImg}
+              name={c.name}
+              width={c.id === 4 || c.id === 5 ? 100 : 150}
+            />
+          ))}
+        </FlowCards>
       </div>
     </section>
   );
