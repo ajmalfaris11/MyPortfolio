5bfab9f699ef82dc17e8652ed0b57213eaa4db62
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-12 09:13:43 +0530

    feat(achievements): enhance LeetCode component layout with CometCard for badges display

--- src/components/Achievements/LeetCode.tsx
+++ src/components/Achievements/LeetCode.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { SparklesText } from "@/components/ui/effects/SparklesText";
+import { CometCard } from "@/components/ui/animations";
 
 export default function LeetCode() {
   return (
-    <div className="bg-transparent">
-      <SparklesText text="Leet Code" />{" "}
+    <div className="bg-transparent p-10">
+      <SparklesText text="Leet Code" />
+      <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-5 pb-10 -mt-15">
+        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
+          <img src="./leetCode/500days.png" alt="500 Days Badge" width={220} />
+        </CometCard>
+
+        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
+          <img src="./leetCode/365days.png" alt="365 Days Badge" width={220} />
+        </CometCard>
+
+        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
+          <img src="./leetCode/200days.png" alt="200 Days Badge" width={220} />
+        </CometCard>
+
+        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
+          <img src="./leetCode/100days.png" alt="100 Days Badge" width={220} />
+        </CometCard>
+
+          <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
+          <img src="./leetCode/100days_2024.png" alt="100 Days Badge" width={220} />
+        </CometCard>
+      </div>
     </div>
   );
 }
