79ff79a834ce135173b8ef7e746ca20adb305c28
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-12 09:22:39 +0530

    refactor(LeetCode): map the leetCode Achiements through single component for cleaner, reusable code

--- src/components/Achievements/LeetCode.tsx
+++ src/components/Achievements/LeetCode.tsx
@@ -1,31 +1,21 @@
 import React from "react";
 import { SparklesText } from "@/components/ui/effects/SparklesText";
 import { CometCard } from "@/components/ui/animations";
+import {leetCodeBadges} from "@/data"
 
 export default function LeetCode() {
   return (
     <div className="bg-transparent p-10">
       <SparklesText text="Leet Code" />
       <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-5 pb-10 -mt-15">
-        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
-          <img src="./leetCode/500days.png" alt="500 Days Badge" width={220} />
-        </CometCard>
-
-        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
-          <img src="./leetCode/365days.png" alt="365 Days Badge" width={220} />
-        </CometCard>
-
-        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
-          <img src="./leetCode/200days.png" alt="200 Days Badge" width={220} />
-        </CometCard>
-
-        <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
-          <img src="./leetCode/100days.png" alt="100 Days Badge" width={220} />
-        </CometCard>
-
-          <CometCard className="w-full h-full text-white flex items-center justify-center font-bold text-xl">
-          <img src="./leetCode/100days_2024.png" alt="100 Days Badge" width={220} />
-        </CometCard>
+        {leetCodeBadges.map((badge, index) => (
+          <CometCard
+            key={index}
+            className="w-full h-full text-white flex items-center justify-center font-bold text-xl"
+          >
+            <img src={badge.card} alt={badge.alt} width="220" />
+          </CometCard>
+        ))}
       </div>
     </div>
   );
--- src/components/ui/animations/index.ts
+++ src/components/ui/animations/index.ts
@@ -3,3 +3,4 @@ export { default as InfiniteCards } from "./InfiniteCards";
 export { default as FlowCards } from "./FlowCards";
 export { default as Animate3DText } from "./Animate3DText";
 export { default as Animate3DDiv } from "./Animate3DDiv";
+export {default as CometCard } from "./comet-card"
