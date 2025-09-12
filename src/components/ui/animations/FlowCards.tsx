1a6e44416e831f61248aec9c5aa31df1ef1426be
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-12 16:09:28 +0530

    refactor(FlowCards): replace items prop with children for improved flexibility

--- src/components/ui/animations/FlowCards.tsx
+++ src/components/ui/animations/FlowCards.tsx
@@ -4,18 +4,13 @@ import { cn } from "@/lib/utils";
 import React, { useEffect, useState } from "react";
 
 function FlowCards({
-  items,
+  children,
   direction = "left",
   speed = "fast",
   pauseOnHover = true,
   className,
 }: {
-  items: {
-    name: string;
-    img: string;
-    nameImg: string;
-    id: number;
-  }[];
+  children: React.ReactNode;
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
   pauseOnHover?: boolean;
@@ -23,20 +18,19 @@ function FlowCards({
 }) {
   const containerRef = React.useRef<HTMLDivElement>(null);
   const scrollerRef = React.useRef<HTMLUListElement>(null);
+  const [start, setStart] = useState(false);
 
   useEffect(() => {
     addAnimation();
   }, []);
-  const [start, setStart] = useState(false);
+
   function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
       const scrollerContent = Array.from(scrollerRef.current.children);
 
       scrollerContent.forEach((item) => {
         const duplicatedItem = item.cloneNode(true);
-        if (scrollerRef.current) {
-          scrollerRef.current.appendChild(duplicatedItem);
-        }
+        scrollerRef.current?.appendChild(duplicatedItem);
       });
 
       getDirection();
@@ -44,76 +38,102 @@ function FlowCards({
       setStart(true);
     }
   }
+
   const getDirection = () => {
     if (containerRef.current) {
-      if (direction === "left") {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "forwards"
-        );
-      } else {
-        containerRef.current.style.setProperty(
-          "--animation-direction",
-          "reverse"
-        );
-      }
+      containerRef.current.style.setProperty(
+        "--animation-direction",
+        direction === "left" ? "forwards" : "reverse"
+      );
     }
   };
+
   const getSpeed = () => {
     if (containerRef.current) {
-      if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "20s");
-      } else if (speed === "normal") {
-        containerRef.current.style.setProperty("--animation-duration", "30s");
-      } else {
-        containerRef.current.style.setProperty("--animation-duration", "40s");
-      }
+      const duration =
+        speed === "fast" ? "20s" : speed === "normal" ? "30s" : "40s";
+      containerRef.current.style.setProperty("--animation-duration", duration);
     }
   };
+
   return (
     <div
       ref={containerRef}
       className={cn(
-        // max-w-7xl to w-screen
-        "scroller relative z-20 w-screen overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        "scroller relative z-20 w-screen overflow-hidden [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
         className
       )}
     >
       <ul
         ref={scrollerRef}
         className={cn(
-          // change gap-16
-          " flex min-w-full shrink-0 gap-16 w-max flex-nowrap",
-          start && "animate-scroll ",
+          "flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap",
+          start && "animate-scroll",
+          pauseOnHover && "hover:[animation-play-state:paused]"
         )}
       >
-        {items.map((item, idx) => (
-          <li
-            className="w-[20vw] max-w-full relative
-             flex justify-center items-center md:w-[15vw]"
-            // change to idx cuz we have the same name
-            key={idx}
-          >
-            <blockquote>
-              <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
-                  src={item.img}
-                  alt={item.name}
-                  className="md:w-10 w-5"
-                />
-                <img
-                  src={item.nameImg}
-                  alt={item.name}
-                  width={item.id === 4 || item.id === 5 ? 100 : 150}
-                  className="md:w-24 w-20"
-                />
-              </div>
-            </blockquote>
-          </li>
-        ))}
+        {children}
       </ul>
     </div>
   );
-};
+}
+
+
+function CompanyCardItem({
+  img,
+  nameImg,
+  name,
+  width,
+}: {
+  img: string;
+  nameImg: string;
+  name: string;
+  width?: number;
+}) {
+  return (
+    <li className="w-[20vw] max-w-full relative flex justify-center items-center md:w-[15vw]">
+      <blockquote>
+        <div className="flex md:max-w-60 max-w-32 gap-2">
+          <img src={img} alt={name} className="md:w-10 w-5" />
+          <img
+            src={nameImg}
+            alt={name}
+            width={width ?? 150}
+            className="md:w-24 w-20"
+          />
+        </div>
+      </blockquote>
+    </li>
+  );
+}
+
+function CertificateCardItem({
+  img,
+  alt,
+  width,
+}: {
+  img: string;
+  alt: string;
+  width?: number;
+}) {
+  return (
+    <li className="w-[30vw] max-w-full relative flex justify-center items-center ">
+      <blockquote>
+        <div className="flex">
+          <img
+            src={img}
+            alt={alt}
+            className="w-full h-auto rounded-3xl shadow-md"
+          />
+        </div>
+      </blockquote>
+    </li>
+  );
+}
+
+
+
 
 export default FlowCards;
+export { CompanyCardItem };
+export { CertificateCardItem };
