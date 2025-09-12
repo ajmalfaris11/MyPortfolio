16c87fd8c05291b041fa972ee2a7a7e5a84b4a43
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-12 16:13:06 +0530

    refactor(FlowCards): export CompanyCardItem alongside FlowCards for improved accessibility

--- src/components/ui/animations/index.ts
+++ src/components/ui/animations/index.ts
@@ -1,6 +1,6 @@
 export { default as AnimatedText } from "./AnimatedText";
 export { default as InfiniteCards } from "./InfiniteCards";
-export { default as FlowCards } from "./FlowCards";
+export { default as FlowCards, CompanyCardItem } from "./FlowCards";
 export { default as Animate3DText } from "./Animate3DText";
 export { default as Animate3DDiv } from "./Animate3DDiv";
 export {default as CometCard } from "./comet-card"
