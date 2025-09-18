2545286afff43f206543801cb6cc9ef47919c79a
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-18 14:26:33 +0530

    add novbar combonent in the servces page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -12,6 +12,7 @@ import {
 import { GoArrowUpRight } from "react-icons/go";
 import CustomCursor from "@/components/Common/CustomCursor";
 import { SparklesCore } from "@/components/ui/effects/sparkles";
+import Navbar from "@/components/Navbar/Navbar";
 import Link from "next/link";
 import { TbIcons } from "react-icons/tb";
 
@@ -93,6 +94,8 @@ export default function ServicesPage() {
           particleColor="#2563eb"
         />
       </div>
+      {/* Top bar (nav-ish visual) */}
+      <Navbar />
 
       {/* Neon ribbon */}
       <div className="w-full bg-blue-600/5 mt-[60px]">
