10b18ec0c195c55870550d3a04fd754d0aef907e
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-22 19:59:40 +0530

    refactor(projects Page): update background from sparkles effect to grid pattern with gradient lines

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,25 +3,29 @@ import Projects from "./projects";
 import CustomCursor from "@/components/Common/CustomCursor";
 import Navbar from "@/components/Navbar/Navbar";
 import Header from "./header";
-import { cn } from "@/lib/utils";
 import Footer from "@/components/Footer/Footer";
+import { SparklesCore } from "@/components/ui/effects/sparkles";
 
 function page() {
   return (
-    <main className="relative w-full p-4 sm:px-6 lg:px-8 bg-black text-white flex flex-col items-center justify-center overflow-hidden mx-auto select-none">
-      <div
-        className={cn(
-          "absolute inset-0",
-          "[background-size:20px_20px]",
-          "[background-image:linear-gradient(to_right,rgba(37,99,235,0.3)_1px,transparent_1px),linear-gradient(to_bottom,rgba(37,99,235,0.3)_1px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,rgba(37,99,235,0.15)_1px,transparent_1px),linear-gradient(to_bottom,rgba(37,99,235,0.15)_1px,transparent_1px)]"
-        )}
-      />
-
+    <main className="h-full relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md scroll-smooth">
+      <div className="w-full h-full absolute inset-0">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#2563eb"
+        />
+      </div>
       <CustomCursor />
       <Navbar />
-      <Header />
+      <section className="p-10">
+        <Header />
       <Projects />
+      </section>
       <Footer />
     </main>
   );
