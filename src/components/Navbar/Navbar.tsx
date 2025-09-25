7c613eed9fce370527a3802aea0075981be1a165
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-25 19:48:23 +0530

    imporve nav bar responsiveness and mobile nav box design

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,18 +19,32 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { SiLeetcode } from "react-icons/si";
 
+import { 
+  FaHome, 
+  FaUserAlt, 
+  FaTools, 
+  FaBriefcase, 
+  FaProjectDiagram, 
+  FaCogs, 
+  FaTrophy, 
+  FaRegComments, 
+  FaFileAlt, 
+  FaEnvelope, 
+  FaSitemap 
+} from "react-icons/fa";
+
 const navLinks = [
-  { name: "Home", link: "/" },
-  { name: "About", link: "/about" },
-  { name: "Skills", link: "/skills" },
-  { name: "Experience", link: "/experience" },
-  { name: "Projects", link: "/projects" },
-  { name: "Services", link: "/services" },
-  { name: "Achievements", link: "/achievements" },
-  { name: "Testimonials", link: "/testimonials" },
-  { name: "Resume", link: "/resume" },
-  { name: "Contact", link: "/contact" },
-  { name: "Site Map", link: "/sitemap" },
+  { name: "Home", link: "/", icon: <FaHome /> },
+  { name: "About", link: "/about", icon: <FaUserAlt /> },
+  { name: "Skills", link: "/skills", icon: <FaTools /> },
+  { name: "Experience", link: "/experience", icon: <FaBriefcase /> },
+  { name: "Projects", link: "/projects", icon: <FaProjectDiagram /> },
+  { name: "Services", link: "/services", icon: <FaCogs /> },
+  { name: "Achievements", link: "/achievements", icon: <FaTrophy /> },
+  { name: "Testimonials", link: "/testimonials", icon: <FaRegComments /> },
+  { name: "Resume", link: "/resume", icon: <FaFileAlt /> },
+  { name: "Contact", link: "/contact", icon: <FaEnvelope /> },
+  { name: "Site Map", link: "/sitemap", icon: <FaSitemap /> },
 ];
 
 export default function Page() {
@@ -53,8 +67,10 @@ export default function Page() {
         <NavbarLogo />
         <NavItems items={navLinks.slice(0, 5)} />
       </NavBody>
-      <Link href="/contact"
-      className="flex justify-center items-center">
+      <Link
+        href="/contact"
+        className="hidden lg:flex justify-center items-center"
+      >
         <span
           className="bg-transparent border-[1.5px] text-white px-6 py-2 rounded-full font-medium 
              hover:bg-blue-700 hover:border-blue-700 transition-colors duration-300 text-nowrap flex justify-center items-center"
@@ -88,34 +104,36 @@ export default function Page() {
 
               {/* Bottom popup */}
               <motion.div
-                className="fixed bottom-0 left-0 w-full max-h-[70vh] bg-gradient-to-b from-neutral-900 to-black rounded-t-4xl shadow-xl z-50 p-6 overflow-y-auto"
+                className="fixed bottom-0 left-0 w-full max-h-[70vh] bg-gradient-to-b from-neutral-900 to-black rounded-t-4xl shadow-xl z-50 pb-6 overflow-y-auto"
                 initial={{ y: "100%" }}
                 animate={{ y: 0 }}
                 exit={{ y: "100%" }}
                 transition={{ type: "spring", stiffness: 300, damping: 30 }}
               >
                 {/* Close button */}
-                <div className="flex  justify-center top-0">
-                  <button
-                    onClick={() => setMenuOpen(false)}
-                    className="w-30 h-1 rounded-full text-2xl font-bold bg-blue-800 transition-colors"
-                  ></button>
+                <div
+                  className="flex justify-center w-full sticky top-0 z-50 bg-neutral-900 backdrop-blur-md rounded-t-4xl p-4"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  <button className="w-24 h-1 rounded-full bg-blue-800 transition-colors" />
                 </div>
 
                 {/* Menu links */}
-                <nav className="flex flex-col text-start gap-5 mt-6">
+                <nav className="flex w-full relative overflow-hidden flex-col text-start gap-5 mt-6 px-6 justify-center ">
                   {navLinks.map((item, idx) => (
                     <Link
                       key={idx}
                       href={item.link}
                       onClick={() => setMenuOpen(false)}
-                      className={`text-lg font-medium text-center py-2 transition-colors ${
+                      className={`text-xl font-medium text-start border-b py-2 transition-colors flex justify-between items-center ${
                         pathname === item.link
-                          ? "text-blue-700 font-semibold  scale-150"
-                          : "text-white hover:bg-white/10"
+                          ? "text-blue-700 font-semibold  border-blue-700"
+                          : "text-white/50 hover:bg-white/10 border-neutral-800"
                       }`}
                     >
                       {item.name}
+
+                      <span className="text-xl">{item.icon}</span>
                     </Link>
                   ))}
                 </nav>
