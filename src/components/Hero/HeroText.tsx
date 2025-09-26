c4f682f46c39b5bef7f11ee6baa4b770f548fc95
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-27 00:35:52 +0530

    Fix (Hero Text) : Disable initial animation on AnimatePresence

--- src/components/Hero/HeroText.tsx
+++ src/components/Hero/HeroText.tsx
@@ -17,17 +17,17 @@ const HeroText: React.FC = () => {
   return (
     <div className="text-center md:text-left flex-2 min-h-[150px]">
       <p className="text-xl">Hello, It&apos;s Me</p>
-     <div className="">
-       <Animate3DText
-        text="Ajmal Faris"
-        className="text-[16vw] md:text-[9vw] xl:text-[10vw] font-bold text-nowrap animate-shine text-start w-full"
-        rotateDepth={10}
-        translateDepth={10}
-      />
-     </div>
+      <div className="">
+        <Animate3DText
+          text="Ajmal Faris"
+          className="text-[16vw] md:text-[9vw] xl:text-[10vw] font-bold text-nowrap animate-shine text-start w-full"
+          rotateDepth={10}
+          translateDepth={10}
+        />
+      </div>
       <div className="flex flex-col md:flex-row justify-start items-center sm:gap-2 text-[8vw] md:text-[2.5vw] lg:text-[3vw]  xl:text-[3.5vw] font-semibold mt-2">
         <span className="text-white">And I'm a</span>
-        <AnimatePresence mode="wait">
+        <AnimatePresence mode="wait" initial={false}>
           <motion.h2
             key={index}
             className="text-blue-600"
@@ -41,7 +41,7 @@ const HeroText: React.FC = () => {
         </AnimatePresence>
       </div>
 
-      <AnimatePresence mode="wait">
+      <AnimatePresence mode="wait" initial={false}>
         <motion.p
           key={index}
           className="mt-4 text-blue-200 max-w-xl text-[4vw] md:text-[2vw]"
