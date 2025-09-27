9273023fc9cb880d1a6c465af1cf8ef846c20960
ajmal faris <ajmalfaris.me@gmail.com>
2025-09-27 21:15:31 +0530

    simplisize the skills card design

--- src/components/Skills/SkillCard.tsx
+++ src/components/Skills/SkillCard.tsx
@@ -10,11 +10,12 @@ interface SkillCardProps {
 export default function SkillCard({ key, icon, label }: SkillCardProps) {
   return (
     <motion.div
-      whileHover={{ scale: 1.1 }}
-      className="flex flex-col items-center justify-center bg-gradient-to-br from-blue-900/30 to-blue-600/10 p-6 rounded-3xl shadow-lg border border-blue-800 hover:border-blue-500 transition-all duration-300"
+      key={key}
+      whileHover={{ scale: 1.05 }}
+      className="flex flex-col items-center justify-center  hover:border-blue-500 transition-all duration-300"
     >
-      <div className="text-5xl mb-3">{icon}</div>
-      <p className="text-lg font-medium text-gray-200">{label}</p>
+      <div className="text-4xl md:text-5xl mb-3">{icon}</div>
+      <p className="text-sm md:text-lg font-medium text-gray-200">{label}</p>
     </motion.div>
   );
 }
